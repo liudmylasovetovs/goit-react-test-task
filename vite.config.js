2b25e996619c5249888e7407decd9372dfaf0f0a
liudmylasovetovs <milkaegik@gmail.com>
2025-03-23 15:39:58 -0700

    vite config versel json package json

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'public'
+  },
   server: {
     historyApiFallback: true
   },
   preview: {
     historyApiFallback: true
   }
-})
+});
+
