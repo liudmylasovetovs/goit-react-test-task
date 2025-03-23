679da9b2e81fbdaa327644f991d7d969f58d4239
liudmylasovetovs <milkaegik@gmail.com>
2025-03-23 15:46:35 -0700

    changes again

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'public'
-  },
   server: {
     historyApiFallback: true
   },
   preview: {
     historyApiFallback: true
   }
-});
-
+})
