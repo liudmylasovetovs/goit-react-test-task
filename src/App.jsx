23f74c3f7ce15ed1a88ba82b7c0ffa1abe4e485b
liudmylasovetovs <milkaegik@gmail.com>
2025-03-24 08:19:10 -0700

    adding favorite page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-// import { lazy, Suspense } from "react";
-// import styles from "./App.module.css";
-
-// import Loader from "./components/Loader/Loader";
-
-// const Home = lazy(() => import("./pages/Home/Home"));
-// const Catalog = lazy(() => import("./pages/Catalog/Catalog"));
-// const Camper = lazy(() => import("./pages/Camper/Camper"));
-// const Features = lazy(() => import("./components/Features/Features"));
-// const Reviews = lazy(() => import("./components/Reviews/Reviews"));
-
-// const App = () => {
-//   return (
-//     <div className={styles.container}>
-//       <Suspense fallback={<Loader />}>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/catalog" element={<Catalog />} />
-//           <Route path="/catalog/:id" element={<Camper />}>
-//             <Route path="features" element={<Features />} />
-//             <Route path="reviews" element={<Reviews />} />
-//           </Route>
-//         </Routes>
-//       </Suspense>
-//     </div>
-//   );
-// };
-
-// export default App;
 import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loader from "./components/Loader/Loader";
@@ -38,6 +8,7 @@ const Catalog = lazy(() => import("./pages/Catalog/Catalog"));
 const Camper = lazy(() => import("./pages/Camper/Camper"));
 const Features = lazy(() => import("./components/Features/Features"));
 const Reviews = lazy(() => import("./components/Reviews/Reviews"));
+const Favorites = lazy(() => import("./pages/Favorites/Favorites")); // ➕ додано
 
 const App = () => {
   return (
@@ -49,6 +20,7 @@ const App = () => {
           <Route path="features" element={<Features />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
+        <Route path="/favorites" element={<Favorites />} /> {/* ➕ додано */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Suspense>
@@ -56,3 +28,4 @@ const App = () => {
 };
 
 export default App;
+
