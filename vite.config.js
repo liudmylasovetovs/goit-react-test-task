5499c4997e566100773d9ed13703b5356411eee4
liudmylasovetovs <milkaegik@gmail.com>
2025-03-23 20:23:32 -0700

    fix vite config 2

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
-  server: {
-    historyApiFallback: true
-  },
-  preview: {
-    historyApiFallback: true
-  }
+  base: '/'
 })
